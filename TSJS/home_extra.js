f7b0331a2aba26b952329663108429e32c0a2fea
Jamie Slaats <jjslaats@mailbox.org>
2019-02-16 16:02:50 +0100

    final version of code review 6

--- TSJS/home_extra.js
+++
@@ -1,6 +0,0 @@
-
-//jQuery Code to be able to set the hight of the card-body equally across the page. 
-jQuery(document).ready(function($){
-        var container_height = $('#dataset').height(); // Get the height of our container
-        $('.travelcard-body').height(container_height);  // Set target DIVs height to container height
-      });
